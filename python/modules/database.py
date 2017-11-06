b434f0141e8a7c87a5a8e7ecb4df712a29d38b9d
Sara04 <sara.sedlar@gmail.com>
2017-11-06 12:31:51 +0100

    [database]: add comments, fix errors

--- python/modules/database.py
+++ python/modules/database.py
@@ -9,22 +9,70 @@ from scan import ScanISLES
 class DatabaseISLES(object):
     """Class for ISLES 2017 database management."""
 
-    def __init__(self, db_path,
-                 modalities=['ADC', 'MTT', 'rCBF',
-                             'rCBV', 'Tmax', 'TTP', 'OT'],
-                 sizes=[128, 192, 256],
-                 classes=[0, 1], n_modalities=6, n_classes=2):
+    """
+        Attributes:
+            db_path: path where training and testing subsets are stored
+            n_classes: number of brain region labels
+            classes: class labels
+                0 - non-lesion
+                1 - stroke lesion
+            n_modalities: number of MRI modalities
+            modalities: list of MRI modalities, namely
+                ADC - apparent diffusion coefitients
+                MTT - mean transit time
+                rCBF - regional cerebral blood flow
+                rCBV - regional cerebral blood volume
+                Tmax - time to maximum
+                TTP - time to peak
+            sizes: list of possible volume's slices' size (width and height)
+            valid_p: percentage of training data that will be used for
+                algorithm's training validation
+
+            train_dict: dictionary for storing training scans
+            test_dict: dictionary for storing test scans
+
+            train_scans: list for storing scans used for algorithm training
+            valid_scans: list for storing scans used for algorithm validation
+
+            Directories:
+            brain_masks_dir: path to the brain's masks
+            normalized_volumes_dir: path to the normalized volumes
+            lesion_dist_dir: path to the lesion distance maps
+            seg_results_dir: path to the segmentation results
+
+        Methods:
+            load_training_dict: creating a dictionary of training scans
+            load_testing_dict: creating a dictionary of testing scans
+            train_valid_split: split training database into train and valid
+                subsets (validation dataset is used for evaluation)
+            name: returns database name with train valid split parameter
+    """
+
+    def __init__(self, db_path, n_classes=2, classes=[0, 1],
+                 n_modalities=6, modalities=['ADC', 'MTT', 'rCBF',
+                                             'rCBV', 'Tmax', 'TTP'],
+                 sizes=[128, 192, 256], valid_p=0.2):
         """Initialization of DatabaseISLES attributes."""
         self.db_path = db_path
-        self.train_dict = {}
-        self.test_dict = {}
-        self.valid_scans = []
-        self.train_scans = []
+        self.n_classes = n_classes
+        self.classes = classes
+        self.n_modalities = n_modalities
         self.modalities = modalities
         self.sizes = sizes
-        self.classes = classes
-        self.n_ms = n_modalities
-        self.n_classes = n_classes
+
+        self.valid_p = valid_p
+
+        self.train_dict = {}
+        self.test_dict = {}
+
+        self.train_scans = []
+        self.valid_scans = []
+
+        self.brain_masks_dir = None
+        self.norm_volumes_dir = None
+        self.augmented_volumes_dir = None
+        self.lesion_dist_dir = None
+        self.seg_results_dir = None
 
     def load_training_dict(self, folder_name='ISLES2017_Training'):
         """Loading training dictionary."""
@@ -45,7 +93,7 @@ class DatabaseISLES(object):
                                                      folder_name, fo))
 
                 for m in modalities:
-                    m_split = str.split(m, '.')
+                    m_split = str.split(str(m), '.')
                     mb = str.split(m_split[-2], '_')[-1]
                     if mb not in modalities_dict:
                         modalities_dict[mb] = m
@@ -76,7 +124,7 @@ class DatabaseISLES(object):
                                                      folder_name, fo))
 
                 for m in modalities:
-                    m_split = str.split(m, '.')
+                    m_split = str.split(str(m), '.')
                     mb = str.split(m_split[-2], '_')[-1]
                     if mb not in modalities_dict:
                         modalities_dict[mb] = m
@@ -88,22 +136,28 @@ class DatabaseISLES(object):
                                                modalities_dict,
                                                'test')
 
-    def train_valid_split(self, split_no, folder_name='ISLES2017_Training'):
+    def train_valid_split(self, folder_name='ISLES2017_Training'):
         """Splitting training data into train and valid subsets."""
         """
             Arguments:
                 folder_name: name of the folder where training data is stored
-
-            Note:
-                the splitting of the training data is done since at the moment
-                of the algorithm creation, testing dataset is not available,
-                so validation dataset would be used for testing
         """
 
+        np.random.seed(123456)
         scan_list = ns.natsort(self.train_dict.keys())
 
-        self.valid_scans = scan_list[split_no * 5:(split_no + 1) * 5]
-        self.train_scans = []
-        for s in scan_list:
-            if s not in self.valid_scans:
+        n_scans = len(self.train_dict)
+        select_valid = np.random.choice(n_scans,
+                                        int(np.round(n_scans *
+                                                     self.valid_p)),
+                                        replace=False)
+
+        for s_idx, s in enumerate(scan_list):
+            if s_idx in select_valid:
+                self.valid_scans.append(s)
+            else:
                 self.train_scans.append(s)
+
+    def name(self):
+        """Return database name."""
+        return "%s(valid_p=%s)" % (type(self).__name__, self.valid_p)
