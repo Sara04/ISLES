5427d61f6926c6b57f12ea7cddcde542e9c49440
Sara04 <sara.sedlar@gmail.com>
2017-11-14 22:35:49 +0100

    [scan]: update; remove meta data extraction

--- python/modules/scan.py
+++ python/modules/scan.py
@@ -1,12 +1,6 @@
 """Class for ISLES 2017 scan/sample info loading and generating."""
 import os
 import nibabel as nib
-import numpy as np
-from scipy.ndimage import morphology
-import cv2
-from scipy.ndimage import interpolation
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 
 
 class ScanISLES(object):
@@ -41,7 +35,7 @@ class ScanISLES(object):
         self.h, self.w, self.d = volume.shape
         return volume
 
-    def load_volumes(self, db, test=False):
+    def load_volumes(self, db, meta, test=False):
         """Loading all volumes as a list numpy arrays."""
         """
             Arguments:
@@ -49,140 +43,8 @@ class ScanISLES(object):
             Returns:
                 list of volumes
         """
-        if test:
-            volumes = [self.load_volume(db, m) for m in db.modalities[:-1]]
-        else:
-            volumes = [self.load_volume(db, m) for m in db.modalities]
-        volumes.append(self.load_brain_mask(db.brain_masks_dir))
-        return volumes
-
-    def load_volumes_norm_aligned(self, db, size, orient=0, test=False):
-
-        volumes = []
-        for m in db.modalities[:-1]:
-            volume_path = os.path.join(db.aug_data_dir,
-                                       self.name + '_' + str(size) +
-                                       '_' + m + '_' + str(orient) + '.bin')
-            v = np.reshape(np.fromfile(volume_path, dtype='float32'),
-                           [size, size, -1])
-            volumes.append(v)
-
+        volumes = [self.load_volume(db, m) for m in db.modalities]
         if not test:
-            volume_path = os.path.join(db.aug_data_dir,
-                                       self.name + '_' + str(size) +
-                                       '_OT_' + str(orient) + '.bin')
-            v = np.reshape(np.fromfile(volume_path, dtype='uint8'),
-                           [size, size, -1])
-            volumes.append(v)
-
-        volume_path = os.path.join(db.aug_data_dir,
-                                   self.name + '_' + str(size) +
-                                   '_brain_mask_' + str(orient) + '.bin')
-        v = np.reshape(np.fromfile(volume_path, dtype='uint8'),
-                       [size, size, -1])
-        volumes.append(v)
-
-        if not test:
-            volume_path = os.path.join(db.lesion_dist_dir,
-                                       self.name + '_' + str(size) +
-                                       '_dist_map_' + str(orient) + '.bin')
-            v = np.reshape(np.fromfile(volume_path, dtype='uint8'),
-                           [size, size, -1])
-            volumes.append(v)
-
+            volumes.append(self.load_volume(db, 'OT'))
+        volumes.append(meta.load_brain_mask(db, self))
         return volumes
-
-    def load_brain_mask(self, exp_out):
-        """Loading brain mask as numpy array."""
-        """
-            Arguments:
-                exp_out: experiment output for meta data
-            Returns:
-                brain mask as a numpy array
-        """
-        brain_mask_path = os.path.join(exp_out,
-                                       self.name + '_brain_mask.bin')
-
-        np_array = np.fromfile(brain_mask_path, dtype='uint8')
-
-        return np.reshape(np_array, (self.h, self.w, self.d))
-
-    def load_lesion_dist_maps(self, exp_out, s):
-        """Loading brain mask as numpy array."""
-        """
-            Arguments:
-                exp_out: experiment output for meta data
-            Returns:
-                brain mask as a numpy array
-        """
-        tdm_path = os.path.join(exp_out, self.name + '_lesion_dist.bin_' + str(s))
-        if os.path.exists(tdm_path):
-            np_array = np.fromfile(tdm_path, dtype='uint8')
-            return np.reshape(np_array, (s, s, self.d))
-        else:
-            return np.zeros((s, s, self.d))
-
-    def _compute_and_save_brain_mask(self, db, brain_mask_path):
-
-        for m in db.modalities[:-1]:
-            volume = self.load_volume(db, m)
-            if 'bm' in locals():
-                bm = np.logical_or(bm, volume != 0)
-            else:
-                bm = volume != 0
-        bm.tofile(brain_mask_path)
-
-    def compute_brain_mask(self, db, exp_out):
-        """Compute brain mask."""
-        """
-            Arguments:
-                db: DatabaseBRATS
-                exp_out: experiment output for meta data
-        """
-        bm_path = os.path.join(exp_out, self.name + '_brain_mask.bin')
-        self._compute_and_save_brain_mask(db, bm_path)
-
-    def _compute_and_save_lesion_distance_map(self, db, brain_mask,
-                                              lesion_distance_map_path):
-
-        v_seg = self.load_volume(db, 'OT')
-        struct_elem = np.ones((3, 3, 3))
-        for s in db.sizes:
-            if v_seg.shape[0] == s:
-                mask = v_seg
-                brain = brain_mask
-            else:
-                mask =\
-                    interpolation.zoom(v_seg,
-                                       [float(s) / self.h,
-                                        float(s) / self.w,
-                                        1]) >= 0.5
-                brain =\
-                    interpolation.zoom(brain_mask,
-                                       [float(s) / self.h,
-                                        float(s) / self.w,
-                                        1]) >= 0.5
-            v = 1.0
-            lesion_dist_map = np.zeros((s, s, self.d))
-            while(np.sum(mask) != s * s * self.d):
-                mask_d = morphology.binary_dilation(mask, struct_elem)
-                lesion_dist_map += (mask_d - mask) * v
-                mask = np.copy(mask_d)
-                v += 1
-
-            lesion_dist_map *= brain
-            lesion_dist_map = np.clip(lesion_dist_map, a_min=0.0, a_max=255.0)
-            lesion_dist_map.astype('uint8').\
-                tofile(lesion_distance_map_path + '_' + str(s))
-
-    def compute_lesion_distance_map(self, db, bm_exp_out, tdm_exp_out):
-        """Lesion distance map computation."""
-        """
-            Arguments:
-                db: DatabaseBRATS
-                exp_out: experiment output for meta data
-        """
-        tdm_path = os.path.join(tdm_exp_out, self.name + '_lesion_dist.bin')
-
-        brain_mask = self.load_brain_mask(bm_exp_out)
-        self._compute_and_save_lesion_distance_map(db, brain_mask, tdm_path)
