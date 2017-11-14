2e5bbbe308cc8c9bf0e85aaa95d98e9fe41f25b2
Sara04 <sara.sedlar@gmail.com>
2017-11-14 22:36:29 +0100

    [modules]: initialization

---
+++ python/modules/__init__.py
@@ -0,0 +1,9 @@
+
+from .database import DatabaseISLES
+from .scan import ScanISLES
+from .preprocessing import PreprocessorISLES
+from .augmentation import AugmentatorISLES
+from .meta_data_extraction import MetaDataExtractorISLES
+from .patch_extraction import PatchExtractorISLES
+from .segmentation import SegmentatorISLES
+from .postprocessing import PostprocessorISLES
